8850d66255eb01d77e940fadee5caf1273889fef
yakomak <yakomak137@gmail.com>
2018-01-24 14:55:46 +0300

    add body-parser

--- index.js
+++ index.js
@@ -1,11 +1,31 @@
 var express = require ('express')
 var cors = require('cors')
+var bodyParser = require('body-parser')
+
 var app = express()
+
+var jsonParser = bodyParser.json()
 
 app.use(cors())
 
 app.get('/',(req,res) => {
   res.send('magtool v.0.1.0')
+})
+
+app.post('/command',jsonParser,(req,res) => {
+  if (!req.body) return res.sendStatus(400)
+  const cmd = req.body.command
+  switch (cmd) {
+    case 'PING':
+      res.json({
+        status: 'OK',
+        data: 'PONG'
+      })
+      break;
+  
+    default:
+      break;
+  }
 })
 
 var port = process.env.PORT || 3000
